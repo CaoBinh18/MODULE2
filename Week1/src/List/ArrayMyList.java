220092d14f299769044dd66e450fed8901e9fced
Cao Binh <binhcao18@gmail.com>
2021-06-16 15:31:34 +0700

    add MyGenericStack

---
+++ Week1/src/List/ArrayMyList.java
@@ -0,0 +1,14 @@
+package List;
+
+public class ArrayMyList<E> {
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object elements[];
+
+    public ArrayMyList() {
+    }
+
+    public ArrayMyList(int capacity) {
+        
+    }
+}
