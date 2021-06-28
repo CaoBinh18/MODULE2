fb6e3b72f66d12cbf56b895bfa9f6fad18b5d1f9
Cao Binh <binhcao18@gmail.com>
2021-06-28 21:31:52 +0700

    add PhoneExample

---
+++ Week1/src/StringRegex/ValidatePhone/PhoneExampleTest.java
@@ -0,0 +1,19 @@
+package StringRegex.ValidatePhone;
+
+public class PhoneExampleTest {
+    private static PhoneExample phoneExample;
+    public static final String[] validPhone = new String[] {"(84)-(0978489648)"};
+    public static final String[] invalidPhone = new String[] {"(a8)-(22222222)"};
+
+    public static void main(String[] args) {
+        phoneExample = new PhoneExample();
+        for (String phone : validPhone) {
+            boolean isvalid = phoneExample.validate(phone);
+            System.out.println("Phone is " + phone + " is valid: " + isvalid);
+        }
+        for (String phone : invalidPhone) {
+            boolean isvalid = phoneExample.validate(phone);
+            System.out.println("Phone is " + phone + " is valid: " + isvalid);
+        }
+    }
+}
