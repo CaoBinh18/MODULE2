35c8a70b3b2dc1e2c274fc1b722af8bc3cd9a6b1
Cao Binh <binhcao18@gmail.com>
2021-06-28 15:25:02 +0700

    add AccountExample

--- Week1/src/StringRegex/ValidateEmail/EmailExample.java
+++ Week1/src/StringRegex/ValidateEmail/EmailExample.java
@@ -9,6 +9,13 @@ public class EmailExample {
 
     private static final String EMAIL_REGEX = "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
 
+    // ^                     Bắt đầu chuỗi String hoặc 1 dòng.
+    // [A-Za-z0-9]+          Email address phải bắt đầu với “[A-Za-z0-9]+” và có ít nhất một ký tự.
+    // [A-Za-z0-9]*@         Tùy chọn theo "[A-Za-z0-9]", và kết thúc bằng một ký hiệu "@".
+    // [A-Za-z0-9]           Domain phải sau @ và ký tự phải nằm trong [A-Za-z0-9].
+    // \\.[A-Za-z0-9]        Sau domain là phần mở rộng của domain sau dấu chấm, ví dụ: (.com, .net, .org).
+    // $                     Kết thúc chuỗi String hoặc kết thúc dòng.
+
     public EmailExample() {
         pattern = Pattern.compile(EMAIL_REGEX);
     }
