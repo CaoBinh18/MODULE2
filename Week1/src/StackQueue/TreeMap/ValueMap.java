0cae44d7e46b324a5970f324d9c696e59f14f0ba
CaoBinh18 <83208816+CaoBinh18@users.noreply.github.com>
2021-07-02 16:26:07 +0700

    add ValueMap

---
+++ Week1/src/StackQueue/TreeMap/ValueMap.java
@@ -0,0 +1,31 @@
+package StackQueue.TreeMap;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class ValueMap {
+    public static String countWord(String str)
+    {
+        String[] arr = str.split("");
+        TreeMap<String, Integer> countWord = new TreeMap<String,Integer>();
+        int value = 1;
+        for(int i = 0; i < str.length(); i++){
+            String key = arr[i];
+            if(countWord.containsKey(key)){
+                countWord.remove(key);
+                countWord.put(key,value + 1);
+            }else {
+                countWord.put(key,value);
+            }
+        }
+
+        return String.valueOf(countWord);
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Nhập chuỗi:");
+        String str = input.nextLine();
+        System.out.println(countWord(str));
+    }
+}
